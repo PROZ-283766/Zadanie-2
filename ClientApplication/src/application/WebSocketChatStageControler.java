dbe623bfc14efd628a25ec9a57683385a9896b87
PROZ-283766 <37350580+PROZ-283766@users.noreply.github.com>
2018-04-27 02:16:20 +0200

    Add files via upload

--- ClientApplication/src/application/WebSocketChatStageControler.java
+++ ClientApplication/src/application/WebSocketChatStageControler.java
@@ -1,5 +1,6 @@
 package application;
 
+import java.io.File;
 import java.io.IOException;
 import java.net.URI;
 
@@ -16,8 +17,12 @@ import javax.websocket.WebSocketContainer;
 
 import javafx.fxml.FXML;
 import javafx.scene.control.Button;
+import javafx.scene.control.ListView;
 import javafx.scene.control.TextArea;
 import javafx.scene.control.TextField;
+import javafx.scene.input.KeyCode;
+import javafx.scene.input.KeyEvent;
+import javafx.stage.FileChooser;
 
 public class WebSocketChatStageControler {
 	@FXML
@@ -30,6 +35,12 @@ public class WebSocketChatStageControler {
 	Button btnSet;
 	@FXML
 	Button btnSend;
+	@FXML
+	Button btnAttach;
+	@FXML
+	Button btnSendAttachment;
+	@FXML
+	ListView<String> attach_ListView;
 	private String user;
 	private WebSocketClient webSocketClient;
 
@@ -38,25 +49,61 @@ public class WebSocketChatStageControler {
 		webSocketClient = new WebSocketClient();
 		user = userTextField.getText();
 		btnSend.setDisable(true);
+		btnAttach.setDisable(true);
+		btnSendAttachment.setDisable(true);
 	}
 
 	@FXML
 	private void btnSet_Click() {
 		user = userTextField.getText();
-		if (userTextField.getText().isEmpty()||user.trim().isEmpty()) {
+		if (userTextField.getText().trim().isEmpty() || user.trim().isEmpty()) {
 			return;
 		}
 		btnSet.setDisable(true);
 		btnSend.setDisable(false);
+		btnAttach.setDisable(false);
 		userTextField.setDisable(true);
 	}
 
 	@FXML
 	private void btnSend_Click() {
-		if(messageTextField.getText().isEmpty())
+		sendMessage();
+	}
+
+	@FXML
+	private void keyEntered(KeyEvent event) {
+		if (event.getCode() == KeyCode.ENTER) {
+			sendMessage();
+		}
+	}
+
+	private void sendMessage() {
+		if (messageTextField.getText().trim().isEmpty())
 			return;
 		webSocketClient.sendMessage(messageTextField.getText());
 		messageTextField.clear();
+	}
+
+	@FXML
+	private void btnAttach_Click() {
+		FileChooser fileChooser = new FileChooser();
+		fileChooser.setTitle("Open Resource File");
+		File selectedFile = fileChooser.showOpenDialog(null);
+		if (selectedFile != null) {
+			System.out.println(selectedFile);
+			webSocketClient.attach(selectedFile.getPath());
+		}
+		if (btnSendAttachment.isDisable() && !attach_ListView.getItems().isEmpty()) {
+			btnSendAttachment.setDisable(false);
+		}
+	}
+
+	@FXML
+	private void btnSendAttachment_Click() {
+
+		webSocketClient.sendFiles();
+
+		attach_ListView.getItems().clear();
 	}
 
 	public void closeSession(CloseReason closeReason) {
@@ -112,17 +159,29 @@ public class WebSocketChatStageControler {
 			try {
 				System.out.println("Message was sent: " + message);
 				session.getBasicRemote().sendText(user + ": " + message);
+
 			} catch (IOException ex) {
 				ex.printStackTrace();
 			}
 		}
-		
-		
-		
-		
-		
-		
-		
-		
+
+		public void attach(String message) {
+			System.out.println("Sth was attached: " + message);
+			attach_ListView.getItems().add(message);
+		}
+
+		public void sendFiles() {
+			try {
+				attach_ListView.getItems().size();
+				for (int i = 0; i != attach_ListView.getItems().size(); ++i) {
+					System.out.println(user + " send: " + attach_ListView.getItems().get(i).toString());
+					session.getBasicRemote().sendText(user + ": " + attach_ListView.getItems().get(i));
+				}
+
+			} catch (IOException ex) {
+				ex.printStackTrace();
+			}
+		}
+
 	} // public class WebSocketClient
 } // public class WebSocketChatStageControler
